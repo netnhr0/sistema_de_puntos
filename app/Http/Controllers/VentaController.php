e45f42eb05f0bb6f8fd91013bf02888bbb86c9b5
netnhr0 <arancibiai34@gmail.com>
2022-04-06 23:05:01 -0400

    Funcion anular Venta, Funcion de Puntos

--- app/Http/Controllers/VentaController.php
+++ app/Http/Controllers/VentaController.php
@@ -2,19 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Venta;
+use App\Model\Venta;
+use App\Model\Comercio;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class VentaController extends Controller
 {
     
     public function insertVenta(){
-        $rut = "99999999-9";
+        $rut = "9999999-9";
         $id_dispositivo = "1111";
         $monto = "149000";
-        $codigo_seguridad = Hash::make($this->code_segurity());
+        $code_segurity = $this->code_segurity();
+        $codigo_seguridad = Hash::make($code_segurity);
         $estado= true;
+        echo "Codigo Seguridad: ".$code_segurity."<br>";
 
         $venta = new Venta();
         $venta->rut = $rut;
@@ -22,7 +26,30 @@ class VentaController extends Controller
         $venta->monto = $monto;
         $venta->codigo_seguridad = $codigo_seguridad;
         $venta->estado = $estado;
-        $venta->save();
+        if ($venta->save()) {
+            echo "registro";
+            $this->puntosComercio('vendio', $rut);
+        }else {
+            echo "no registro";
+        }
+    }
+
+    public function anularVenta(){
+        $rut = "9999999-9";
+        $ventas = Venta::select('id_venta', 'codigo_seguridad')->where('rut', $rut)->get();
+        
+        foreach ($ventas as $venta) {
+            $id_venta = $venta->id_venta;
+            $codigo_seguridad = $venta->codigo_seguridad;
+            $codes = Hash::check('4295643726', $codigo_seguridad);
+            if ($id_venta == 13 && $codes) {
+                Venta::where('id_venta', $id_venta)->update(['estado' => false]);
+                $this->puntosComercio("anular", $rut);
+                echo "<br>se actualizo";
+            }else{
+                echo "<br>el id_venta y el codigo de Seguridad no son correcto";
+            }
+        }
     }
 
     private function code_segurity(){
@@ -34,6 +61,20 @@ class VentaController extends Controller
             }
         }
         return $code;
+    }
+
+    private function puntosComercio($operacion, $rut){
+        $puntosDB = DB::select('select puntos from comercios where rut = :rut', ['rut' => $rut]);
+        foreach ($puntosDB as $puntos) {
+            $punto = $puntos->puntos;
+        }
+        if ($operacion == "vendio") {
+            $punto = $punto+10;
+            Comercio::where('rut', $rut)->update(['puntos' => $punto]);
+        }else if($operacion == "anular"){
+            $punto = $punto-10;
+            Comercio::where('rut', $rut)->update(['puntos' => $punto]);
+        }
     }
 
     
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'VentaController@insertVenta');
+Route::get('/', 'VentaController@anularVenta');
